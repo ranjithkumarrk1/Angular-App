c5305e6e675a7aa8e958b671cb5bafe131e5aca3
ranjithkumar <msranjithkumarmec@gmail.com>
2023-09-10 20:34:23 +0530

    Font view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  styleUrls: ['./app.component.less'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    this.addToggleAnimation();
+  }
   title = 'Angular-App';
+
+  addToggleAnimation(): void {
+    const toggleBtn = document.getElementById('nav-toggle');
+    toggleBtn?.addEventListener('click', () => {
+      if (!toggleBtn.classList.contains('is-active')) {
+        toggleBtn.classList.add('is-active');
+      } else {
+        toggleBtn.classList.remove('is-active');
+      }
+      const nav = document.getElementsByClassName('nav')[0];
+      if (!nav.classList.contains('show')) {
+        nav.classList.add('show');
+      } else {
+        nav.classList.remove('show');
+      }
+    });
+  }
 }
